d9383be3f9ac265c93746d65e7c393b4ea1fbd44
Abhishek Kushwaha <92770059+abhishekkushwahaa@users.noreply.github.com>
2024-10-01 08:13:51 +0530

    Print Star Triangle

--- 4.star_triangle.go
+++ 4.star_triangle.go
@@ -1,5 +1,12 @@
 package main
 
-func printStarTriangle() {
+import "fmt"
 
+func printStarTriangle() {
+	for i := 0; i <= 5; i++ {
+		for j := 0; j < i; j++ {
+			fmt.Print("* ")
+		}
+		fmt.Println()
+	}
 }
